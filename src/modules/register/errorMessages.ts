89090c8b686ddfdbbe2a7b54b85e6b4f3c1f6e0d
Tyler Smith <tyler.smith.la@gmail.com>
2018-05-15 22:11:39 -0400

    GraphQL validating input

---
+++ src/modules/register/errorMessages.ts
@@ -0,0 +1,4 @@
+export const duplicateEmail = "already taken";
+export const emailNotLongEnough = "email must be atleast 3 characters";
+export const invalidEmail = "email must be a valid email";
+export const passwordNotLongEnough = "password must be atleast 3 characters";
